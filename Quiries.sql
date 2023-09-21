899a57c7840d4aad6eaf3c07cab86050a35b8aea
ArmandoMagallanJr <ajmag7@sbcglobal.net>
2023-09-20 19:43:24 -0500

    updated repo

--- Quiries.sql
+++ Quiries.sql
@@ -1,45 +1,45 @@
--- 1:
+--Quiry 1:
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
---2: 
+--Quiry 2: 
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
---3: 
+--Quiry 3: 
 SELECT dm.dept_no, dm.emp_no, e.last_name, e.first_name, d.dept_name
 FROM dept_manager dm
 INNER JOIN employees e ON dm.emp_no = e.emp_no
 INNER JOIN departments d ON dm.dept_no = d.dept_no;
 
---4: 
+--Quiry 4: 
 SELECT  de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 INNER JOIN dept_emp de ON e.emp_no = de.emp_no
 INNER JOIN departments d ON de.dept_no = d.dept_no;
 
---5: 
+--Quiry 5: 
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 
---6: 
+--Quiry 6: 
 SELECT e.emp_no, e.last_name, e.first_name
 FROM employees e
 INNER JOIN dept_emp de ON e.emp_no = de.emp_no
 INNER JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
---7: 
+--Quiry 7: 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 INNER JOIN dept_emp de ON e.emp_no = de.emp_no
 INNER JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name IN ('Sales', 'Development');
 
---8: 
+--Quiry 8: 
 SELECT last_name, COUNT(*) as count
 FROM employees
 GROUP BY last_name
